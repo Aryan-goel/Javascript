e2ae8f0332b4c423522a0804060565632c2b2138
91639 <goel.aryan2406@gmail.com>
2022-04-03 16:52:56 +0530

    closures

--- timeOuts&closures.js
+++ timeOuts&closures.js
@@ -1,9 +1,15 @@
 function x() {
-  var i = 1;
-  setTimeout(function () {
-    console.log(i);
-  }, 1000);
+  for(var i=1;i<=5;i++){//! it did not work with var and did with let is bcz let is blockscoped
+    function close(i){
+    setTimeout(function () {
+        console.log(i);
+      },i*1000);
+    }
+    close(i);//? this works with var this time bcz the i in the close function creates a new copy of i everytime(iteration) 
+  }
+  
   console.log("ksdfs");
 }
 x();
- 
+//! clousures can have a over consupmtion of memory
+  
