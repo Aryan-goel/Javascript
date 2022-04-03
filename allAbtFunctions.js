2bd9869a1fe20f1d7e743d8e59396e3c9ea8ed56
91639 <goel.aryan2406@gmail.com>
2022-04-03 17:58:18 +0530

    functions

---
+++ allAbtFunctions.js
@@ -0,0 +1,30 @@
+//*function stmnt
+a();
+function a() {
+  console.log("a called");
+}
+
+
+//! the diffrence b/w functio  n stmnt and exp is hoisting
+
+
+//*fucntion exp
+b();
+var b = function () {
+  //? here the function acts like a value
+  console.log("b called");
+};
+
+
+
+//*function decleration
+
+//*anonymous funtion
+
+//*names function exp
+
+//*diffrebce b/w parameters and arguments
+
+//*first class functions
+
+//*arrow functions
