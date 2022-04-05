47d62b691c33babee6815a0ab4bd33cfaa03ae96
91639 <goel.aryan2406@gmail.com>
2022-04-05 12:13:09 +0530

    eventListeners

--- callbackFuntions.js
+++ callbackFuntions.js
@@ -1,12 +1,20 @@
-//* call back functions 
+//* call back functions
 function x(y) {
-console.log('x');
-y();
+  console.log("x");
+  y();
 }
-x( function y(){
-    console.log('y');
-})
+x(function y() {
+  console.log("y");
+});
 
 setTimeout(function () {
-    console.log("timer")
-},5000)
+  console.log("timer");
+}, 5000);
+
+function attach() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("button clicked", count++); //? the function xyz is the handler function of the button
+  });
+}
+attach();
