4c829b01797656fc89d134cdbf9c936985263c72
91639 <goel.aryan2406@gmail.com>
2022-04-07 12:24:12 +0530

    js engine

---
+++ eventLoops.js
@@ -0,0 +1,13 @@
+/*
+ setTimeout()
+ DOM Api's
+ fetch()
+ localStorage
+ Console
+ location
+
+
+these are all the web api's and js just gives us access to use them on the js engine of the browser
+using the window keyword that is.
+  we can still use all of them without using the keyword windown if they are in the global scope
+*/
