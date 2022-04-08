ee128d2f35cfa0cfeb8a92b165b87a41463f1e8c
91639 <goel.aryan2406@gmail.com>
2022-04-08 13:17:32 +0530

    microtasks

--- eventLoops.js
+++ eventLoops.js
@@ -1,4 +1,4 @@
-/*
+/* 
  setTimeout()
  DOM Api's
  fetch()
@@ -11,3 +11,9 @@ these are all the web api's and js just gives us access to use them on the js en
 using the window keyword that is.
   we can still use all of them without using the keyword windown if they are in the global scope
 */
+
+//? The use of event loop is to put things inside the callback queue into the call stack.
+  
+//? tasks inside the microtask queue are of higher priority then the call back queue
+
+//?all the callback funtion that come through promises will go inside microftask queue
